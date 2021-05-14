540a61c20a6e3dc4ab7259a07df90b37bfd64f92
Freddy L P.B <freddy_luis_94@hotmail.com>
2021-05-14 13:54:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Express + Typescript server template
+
+Create a server using:
+
+- Express
+- Typescript
+
+---
+
+## Requires degit
+
+```bash
+npm install -g degit
+```
+
+## Download template
+
+```bash
+degit Neo-Ciber94/template-express
+```
+
+## Install all the required node modules
+
+```bash
+npm install
+```
+
+## Run
+
+```bash
+yarn dev
+```
+
+Or
+
+```bash
+npm run dev
+```
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import fs from "fs";
+
+// Port to run the server
+const PORT = process.env.PORT || 8000;
+
+// Create the express app
+const app = express();
+
+// Dynamically import all routes in src/routes
+fs.readdir(__dirname + "/routes", (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  for (const file of files) {
+    app.use("/", require(__dirname + "/routes/" + file).default);
+  }
+});
+
+// Setup middlewares
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Start listening
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
